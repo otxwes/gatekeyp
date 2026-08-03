82c2eace9a49f4986015096b96a985a63d3db37f
otx wes <otxwes@gmail.com>
2026-08-02 20:40:43 -0400

    Export ContentManager and related classes from src.core package

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,1 +1,17 @@
 # Copyright (c) 2026 Gatekeyp contributors
+
+from src.core.content_manager import (
+    ALLOWED_MIME_TYPES,
+    ContentAccessError,
+    ContentManager,
+    ContentValidationError,
+)
+from src.core.key_manager import KeyManager
+
+__all__ = [
+    "ALLOWED_MIME_TYPES",
+    "ContentAccessError",
+    "ContentManager",
+    "ContentValidationError",
+    "KeyManager",
+]
